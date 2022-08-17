e6018796595bf27273509b7273db415f9b4b15dc
AhmedMahfouz <ahmelsamahy@gmail.com>
2022-08-17 17:37:33 +0200

    feat<>: infinite line generate

--- main.py
+++ main.py
@@ -7,18 +7,20 @@ from defs import *
 from itertools import cycle
 
 clock = pygame.time.Clock()
+WHITE = (255, 255, 255)
 LINE_GAP = 0
 SCORE_COUNTER = 0
 DELTA_TIME = 0
 GAME_TIME = 0
 GAME_COUNTER = 0
-CORD_Y= 0
-CORD_X= 0
-Y_AXIS_INCREMENT=0
+CORD_Y = 0
+CORD_X = 0
+Y_AXIS_INCREMENT = 0
 POINTS_MATRIX = [[0 for x in range(2)] for y in range(DISPLAY_H)]
 posX = cycle(range(2))
 pygame.init()
-GAME_DISPLAY = pygame.display.set_mode((DISPLAY_W, DISPLAY_H), pygame.NOFRAME) # Make a window to show on
+GAME_DISPLAY = pygame.display.set_mode(
+    (DISPLAY_W, DISPLAY_H), pygame.NOFRAME)  # Make a window to show on
 pygame.display.set_caption('Survive line')
 label_font = pygame.font.Font("./usedMaterial/Nexa-Light.otf", DATA_FONT_SIZE)
 
@@ -27,6 +29,7 @@ def update_label(data, title, font, x, y, GAME_DISPLAY):
     label = font.render('{} {}'.format(title, data), 1, DATA_FONT_COLOR)
     GAME_DISPLAY.blit(label, (x, y))
     return y
+
 
 def update_data_labels(GAME_DISPLAY, DELTA_TIME, GAME_TIME, font):
     y_pos = 10
@@ -37,21 +40,21 @@ def update_data_labels(GAME_DISPLAY, DELTA_TIME, GAME_TIME, font):
     y_pos = update_label(round(GAME_TIME/1000, 2), 'Game time',
                          font, x_pos, y_pos + gap, GAME_DISPLAY)
 
-def generateWave(repeatNum):
-    global CORD_Y
-    for x in range(repeatNum):
-        CORD_Y+=1
-        amplitude = 50
-        frequency = 1
-        speed = 1
-        x = int((DISPLAY_H/2) + amplitude*math.sin(frequency *
-                ((float(CORD_Y)/-DISPLAY_W)*(2*math.pi) + (speed*time.time()))))
-        POINTS_MATRIX[CORD_Y][next(posX)] = x
-        POINTS_MATRIX[CORD_Y][next(posX)] = CORD_Y
 
-# def showWave():
-#         for show in range(DISPLAY_H):
-#             pygame.gfxdraw.pixel(GAME_DISPLAY, POINTS_MATRIX[CORD_Y][next(posX)]-55, POINTS_MATRIX[CORD_Y][next(posX)]+show,(255, 255, 255))
+def generateWave():
+    global CORD_Y
+    if CORD_Y < DISPLAY_H-1:
+        CORD_Y += 1
+    else:
+        CORD_Y = 0
+    amplitude = 50
+    frequency = 1
+    speed = int(FPS/30)
+    x = int((DISPLAY_H/2) + amplitude*math.sin(frequency *
+            ((float(CORD_Y)/-DISPLAY_W)*(2*math.pi) + (speed*time.time()))))
+    POINTS_MATRIX[CORD_Y][next(posX)] = x
+    POINTS_MATRIX[CORD_Y][next(posX)] = -SCORE_COUNTER
+
 
 def debug(SCORE_COUNTER, LINE_GAP):
     global CORD_Y
@@ -60,37 +63,30 @@ def debug(SCORE_COUNTER, LINE_GAP):
         LINE_GAP += 1
         print("Incremented line gap")
 
-        
-
 
 def run_game():
     # Make a surface to draw on
     surface = pygame.Surface((DISPLAY_W, DISPLAY_H))
     surface.fill(background_color)
     running = True
-    generateWave(799)
+
     while running:
-        DELTA_TIME = clock.tick(30)
+        DELTA_TIME = clock.tick(FPS)
         # GAME_TIME += DELTA_TIME
         surface.fill(background_color)
-
 
         # if (scoreCounter % 5 == 0):
         #     frequency = random.randint(1, 6)
 
-        global SCORE_COUNTER,GAME_COUNTER
+        global SCORE_COUNTER, GAME_COUNTER
         SCORE_COUNTER += 1
         debug(SCORE_COUNTER, LINE_GAP)
-        
-        
-        for Y_CORD in range(500):
-            pygame.gfxdraw.pixel(GAME_DISPLAY, POINTS_MATRIX[Y_CORD][next(posX)]-55, POINTS_MATRIX[Y_CORD][next(posX)]+SCORE_COUNTER,(255, 255, 255))
+
+        generateWave()
+
+        for Y_CORD in range(800):
+            pygame.gfxdraw.pixel(GAME_DISPLAY, POINTS_MATRIX[Y_CORD][next(posX)]-55, POINTS_MATRIX[Y_CORD][next(posX)]+SCORE_COUNTER, WHITE)
         pygame.display.flip()
-
-        # for show in range(0,DISPLAY_H):
-        #     pygame.gfxdraw.pixel(GAME_DISPLAY, POINTS_MATRIX[y][next(posX)]-55, POINTS_MATRIX[y][next(posX)]+show, (255, 255, 255))
-
-        
 
         if (SCORE_COUNTER % 1 == 0):
             GAME_DISPLAY.blit(surface, (0, 0))
