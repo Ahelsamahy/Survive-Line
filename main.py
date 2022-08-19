0fd2743fda42f44d651b0cf5e5e2ade070e6d3b5
AhmedMahfouz <ahmelsamahy@gmail.com>
2022-08-19 15:11:13 +0200

    feat<>: create ball movement

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from itertools import cycle
 
 clock = pygame.time.Clock()
 WHITE = (255, 255, 255)
-WAVE_COLOUR= (178, 190, 181)
+WAVE_COLOUR = (178, 190, 181)
 WAVE_GAP = 0
 SCORE_COUNTER = 0
 DELTA_TIME = 0
@@ -24,6 +24,12 @@ WAVE_CORD_Y = 0
 WAVE_CORD_X = 0
 POINTS_MATRIX = [[0 for x in range(2)] for y in range(DISPLAY_H)]
 posX = cycle(range(2))
+
+BALL_CORD_Y = 550
+BALL_CORD_X = DISPLAY_W//2
+BALL_MOVE_SPEED = 3
+BALL_RADIUS = 12
+
 pygame.init()
 GAME_DISPLAY = pygame.display.set_mode(
     (DISPLAY_W, DISPLAY_H), pygame.NOFRAME)  # Make a window to show on
@@ -62,7 +68,8 @@ def fillGap(gap, gapDirection):
         POINTS_MATRIX[WAVE_CORD_Y + (gap)][0] = POINTS_MATRIX[WAVE_CORD_Y][0]
         POINTS_MATRIX[WAVE_CORD_Y + (gap)][1] = POINTS_MATRIX[WAVE_CORD_Y][1]
         POINTS_MATRIX[WAVE_CORD_Y][0] = POINTS_MATRIX[WAVE_CORD_Y][1] = 0
-        print(POINTS_MATRIX[WAVE_CORD_Y-1][0], POINTS_MATRIX[WAVE_CORD_Y+gap][0])
+        print(POINTS_MATRIX[WAVE_CORD_Y-1][0],
+              POINTS_MATRIX[WAVE_CORD_Y+gap][0])
         for x in range(POINTS_MATRIX[WAVE_CORD_Y-1][0]+1, POINTS_MATRIX[WAVE_CORD_Y+gap][0], (gap//gap)):
             POINTS_MATRIX[insideY][next(posX)] = x
             POINTS_MATRIX[insideY][next(posX)] = -SCORE_COUNTER+1
@@ -72,13 +79,14 @@ def fillGap(gap, gapDirection):
         POINTS_MATRIX[WAVE_CORD_Y + (gap)][0] = POINTS_MATRIX[WAVE_CORD_Y][0]
         POINTS_MATRIX[WAVE_CORD_Y + (gap)][1] = POINTS_MATRIX[WAVE_CORD_Y][1]
         POINTS_MATRIX[WAVE_CORD_Y][0] = POINTS_MATRIX[WAVE_CORD_Y][1] = 0
-        print(POINTS_MATRIX[WAVE_CORD_Y-1][0], POINTS_MATRIX[WAVE_CORD_Y+gap][0])
+        print(POINTS_MATRIX[WAVE_CORD_Y-1][0],
+              POINTS_MATRIX[WAVE_CORD_Y+gap][0])
         for x in range(POINTS_MATRIX[WAVE_CORD_Y-1][0]-1, POINTS_MATRIX[WAVE_CORD_Y+gap][0], -(gap//gap)):
             POINTS_MATRIX[insideY][next(posX)] = x
             POINTS_MATRIX[insideY][next(posX)] = -SCORE_COUNTER
             if insideY < 799:
                 insideY += 1
-    print("Moved a point")
+    # print("Moved a point")
 
 
 def checkGap():
@@ -87,7 +95,8 @@ def checkGap():
         gap = (POINTS_MATRIX[WAVE_CORD_Y-1][0]-POINTS_MATRIX[WAVE_CORD_Y][0])-1
         fillGap(gap, False)
     elif(POINTS_MATRIX[WAVE_CORD_Y][0] - POINTS_MATRIX[WAVE_CORD_Y-1][0] > 1) and (WAVE_CORD_Y not in {1, 2, 3}):
-        gap = (POINTS_MATRIX[WAVE_CORD_Y][0] - POINTS_MATRIX[WAVE_CORD_Y-1][0]) - 1
+        gap = (POINTS_MATRIX[WAVE_CORD_Y][0] -
+               POINTS_MATRIX[WAVE_CORD_Y-1][0]) - 1
         fillGap(gap, True)
 
 
@@ -100,7 +109,7 @@ def generateWave():
         WAVE_CORD_Y = 0
 
     WAVE_CORD_X = int((DISPLAY_H/2) + WAVE_AMPLITUDE*math.sin(WAVE_FREQUENCY *
-                 ((float(WAVE_CORD_Y)/-DISPLAY_W)*(2*math.pi) + (WAVE_SPEED*time.time()))))
+                                                              ((float(WAVE_CORD_Y)/-DISPLAY_W)*(2*math.pi) + (WAVE_SPEED*time.time()))))
     if WAVE_CORD_X-WAVE_AMPLITUDE-WAVE_GAP > DISPLAY_W:
         WAVE_CORD_X = DISPLAY_W+50+WAVE_GAP
     elif WAVE_CORD_X-350-WAVE_GAP > DISPLAY_W:
@@ -115,7 +124,7 @@ def generateWave():
 
 def changeSpeed():
     global FPS, WAVE_GAP, GAME_SPEED
-    if(SCORE_COUNTER % (100 * (GAME_SPEED//2)) == 0) and FPS < 120:
+    if(SCORE_COUNTER % (100 * (GAME_SPEED//2)) == 0) and FPS < 60:
         FPS += 2
         GAME_SPEED *= GAME_SPEED
         print("incremented speed of game")
@@ -123,16 +132,30 @@ def changeSpeed():
 
 def changeWave():
     global WAVE_FREQUENCY, WAVE_AMPLITUDE, WAVE_SPEED, WAVE_GAP
-    if (SCORE_COUNTER % 10 == 0) and WAVE_GAP < 60:
+    if (SCORE_COUNTER % 10 == 0) and WAVE_GAP < 80:
         WAVE_GAP += 1
         print("Incremented line gap")
-    if (SCORE_COUNTER % 101 == 0):
+    if (SCORE_COUNTER % 200 == 0):
         # WAVE_FREQUENCY = random.randint(1, 6)
         WAVE_AMPLITUDE = random.randint(50, WAVE_AMPLITUDE+WAVE_GAP)
 
 
 def debug(SCORE_COUNTER):
     print(SCORE_COUNTER)
+
+
+def drawCircle(surface, x, y, radius, color):
+    pygame.gfxdraw.aacircle(surface, x, y, radius, color)
+    pygame.gfxdraw.filled_circle(surface, x, y, radius, color)
+
+
+def moveCircle():
+    global BALL_CORD_X
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT] and BALL_CORD_X + (BALL_RADIUS) < DISPLAY_W-4:
+        BALL_CORD_X += BALL_MOVE_SPEED
+    if keys[pygame.K_LEFT] and BALL_CORD_X > 0 + BALL_RADIUS+4:
+        BALL_CORD_X -= BALL_MOVE_SPEED
 
 
 def run_game():
@@ -146,9 +169,9 @@ def run_game():
         # GAME_TIME += DELTA_TIME
         surface.fill(background_color)
 
-        global SCORE_COUNTER, GAME_COUNTER
+        global SCORE_COUNTER, GAME_COUNTER, BALL_CORD_X, BALL_CORD_Y
         SCORE_COUNTER += 1
-        debug(SCORE_COUNTER, WAVE_GAP)
+        debug(SCORE_COUNTER)
 
         changeSpeed()
         changeWave()
@@ -169,19 +192,21 @@ def run_game():
             pygame.gfxdraw.pixel(GAME_DISPLAY, POINTS_MATRIX[Y_CORD][next(
                 posX)]-350+WAVE_GAP-1, POINTS_MATRIX[Y_CORD][next(posX)]+SCORE_COUNTER, WAVE_COLOUR)
 
-
-
-        if (SCORE_COUNTER % 1 == 0):
-            GAME_DISPLAY.blit(surface, (0, 0))
         for event in pygame.event.get():
             if (event.type == pygame.QUIT):
                 running = False
-
             elif event.type == pygame.KEYDOWN:
                 if(event.key == pygame.K_ESCAPE):
                     running = False
                     print("ESC key is pressed, will stop now\n")
-                if event.key == pygame.K_RIGHT:
+
+        moveCircle()
+        drawCircle(GAME_DISPLAY, BALL_CORD_X, BALL_CORD_Y, BALL_RADIUS, WHITE)
+
+        pygame.display.flip()
+
+        if (SCORE_COUNTER % 1 == 0):
+            GAME_DISPLAY.blit(surface, (0, 0))
 
         # update_data_labels(GAME_DISPLAY, DELTA_TIME, GAME_TIME, label_font)
 
