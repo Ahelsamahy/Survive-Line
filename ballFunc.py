cab8d100d5a2408d0149919f8b3027fc39b9c8ad
AhmedMahfouz <ahmelsamahy@gmail.com>
2022-10-12 00:21:00 +0200

    feat<>: move ball related function to its own file
    with the fixed points to the defs file

---
+++ ballFunc.py
@@ -0,0 +1,64 @@
+
+from tkinter import FALSE
+from numpy import random
+from defs import *
+
+from mainL import *
+
+import pygame
+
+PARTICLES = []
+BALL_CORD_Y = 550
+BALL_CORD_X = DISPLAY_W//2
+BALL_MOVE_SPEED = 5
+BALL_RADIUS = 12
+
+def collision():
+    global keepGenerating
+    ball = pygame.Rect(BALL_CORD_X, BALL_CORD_Y, BALL_RADIUS, BALL_RADIUS)
+    for x in range(245, 255):
+        if ball.right >= POINTS_LIST[x]-55:
+            print("hit from " + str(x))
+            return keepGenerating == FALSE
+
+        if ball.left <= POINTS_LIST[x]-340:
+            print("hit from " + str(x))
+            return keepGenerating == FALSE
+
+
+def drawCircle(SCREEN, x, y, radius, color):
+    pygame.gfxdraw.aacircle(SCREEN, x, y, radius, color)
+    pygame.gfxdraw.filled_circle(SCREEN, x, y, radius, color)
+
+def moveCircle():
+    global BALL_CORD_X
+    PRESSED_KEYS = pygame.key.get_pressed()
+    if PRESSED_KEYS[pygame.K_RIGHT] and BALL_CORD_X + (BALL_RADIUS) < DISPLAY_W-4:
+        BALL_CORD_X += BALL_MOVE_SPEED
+    if PRESSED_KEYS[pygame.K_LEFT] and BALL_CORD_X > 0 + BALL_RADIUS+4:
+        BALL_CORD_X -= BALL_MOVE_SPEED
+    return BALL_CORD_X
+
+def ballParticles():
+    # [loc, velocity, timer]
+    PARTICLES.append([[BALL_CORD_X, BALL_CORD_Y], [
+                     random.randint(0, 20) / 10 - 1, -3], random.randint(4, 6)])
+
+    for particle in PARTICLES:
+        particle[0][0] -= particle[1][0]
+        particle[0][1] -= particle[1][1]
+        particle[2] -= 0.1
+
+        pygame.draw.circle(GAME_DISPLAY, (255, 255, 255), [int(
+            particle[0][0]), int(particle[0][1])], int(particle[2]))
+        if particle[2] <= 0:
+            PARTICLES.remove(particle)
+
+def reset():
+    global BALL_CORD_Y, BALL_CORD_X, POINTS_LIST, keepGenerating, PARTICLES,SCORE_COUNTER
+    BALL_CORD_Y = 550
+    BALL_CORD_X = DISPLAY_W//2
+    SCORE_COUNTER =0
+    POINTS_LIST.clear()
+    keepGenerating = TRUE
+    PARTICLES.clear()
--- mainL.py
+++ mainL.py
@@ -9,33 +9,15 @@ import time
 from numpy import random
 from defs import *
 from itertools import cycle
+from ballFunc import *
 
 clock = pygame.time.Clock()
 SCREEN = pygame.Surface((DISPLAY_W, DISPLAY_H))
 
-WHITE = (255, 255, 255)
-WAVE_COLOUR = (178, 190, 181)
-WAVE_GAP = 0
-SCORE_COUNTER = 0
-DELTA_TIME = 0
-GAME_TIME = 0
-GAME_COUNTER = 0
-GAME_SPEED = 2
+
 # [loc, velocity, timer]
-PARTICLES = []
+
 songNum = 1
-
-WAVE_AMPLITUDE = 50
-WAVE_FREQUENCY = 1
-WAVE_SPEED = 1
-WAVE_CORD_X = 0
-POINTS_LIST = []
-keepGenerating = TRUE
-
-BALL_CORD_Y = 550
-BALL_CORD_X = DISPLAY_W//2
-BALL_MOVE_SPEED = 5
-BALL_RADIUS = 12
 
 pygame.init()
 pygame.mixer.pre_init(44100, -16, 2, 512)
@@ -83,44 +65,8 @@ def changeSpeed():
         print("incremented speed of game")
 
 
-def collision():
-    global keepGenerating
-    ball = pygame.Rect(BALL_CORD_X, BALL_CORD_Y, BALL_RADIUS, BALL_RADIUS)
-    for x in range(245, 255):
-        if ball.right >= POINTS_LIST[x]-55:
-            print("hit from " + str(x))
-            keepGenerating = FALSE
-
-        if ball.left <= POINTS_LIST[x]-340:
-            print("hit from " + str(x))
-            keepGenerating = FALSE
-
-
 def debug(SCORE_COUNTER):
     print(SCORE_COUNTER)
-
-
-def drawCircle(SCREEN, x, y, radius, color):
-    pygame.gfxdraw.aacircle(SCREEN, x, y, radius, color)
-    pygame.gfxdraw.filled_circle(SCREEN, x, y, radius, color)
-
-
-def moveCircle():
-    global BALL_CORD_X
-    PRESSED_KEYS = pygame.key.get_pressed()
-    if PRESSED_KEYS[pygame.K_RIGHT] and BALL_CORD_X + (BALL_RADIUS) < DISPLAY_W-4:
-        BALL_CORD_X += BALL_MOVE_SPEED
-    if PRESSED_KEYS[pygame.K_LEFT] and BALL_CORD_X > 0 + BALL_RADIUS+4:
-        BALL_CORD_X -= BALL_MOVE_SPEED
-
-
-def reset():
-    global BALL_CORD_Y, BALL_CORD_X, POINTS_LIST, keepGenerating, PARTICLES
-    BALL_CORD_Y = 550
-    BALL_CORD_X = DISPLAY_W//2
-    POINTS_LIST.clear()
-    keepGenerating = TRUE
-    PARTICLES.clear()
 
 
 def gameOver():
@@ -156,22 +102,6 @@ def gameOver():
                 else:
                     reset()
                     run_game()
-
-
-def ballParticles():
-    # [loc, velocity, timer]
-    PARTICLES.append([[BALL_CORD_X, BALL_CORD_Y], [
-                     random.randint(0, 20) / 10 - 1, -3], random.randint(4, 6)])
-
-    for particle in PARTICLES:
-        particle[0][0] -= particle[1][0]
-        particle[0][1] -= particle[1][1]
-        particle[2] -= 0.1
-
-        pygame.draw.circle(GAME_DISPLAY, (255, 255, 255), [int(
-            particle[0][0]), int(particle[0][1])], int(particle[2]))
-        if particle[2] <= 0:
-            PARTICLES.remove(particle)
 
 
 def backgroundMusic():
@@ -227,24 +157,21 @@ def run_game():
         DELTA_TIME = clock.tick(FPS)
         # GAME_TIME += DELTA_TIME
         SCREEN.fill(background_color)
-
-        global SCORE_COUNTER, GAME_COUNTER, BALL_CORD_X, BALL_CORD_Y, songNum
+        
+        global SCORE_COUNTER, GAME_COUNTER, BALL_CORD_X, BALL_CORD_Y, songNum, keepGenerating
         SCORE_COUNTER += 1
-
+        keepGenerating = TRUE
         if len(POINTS_LIST) > 260:
-            collision()
+            keepGenerating = collision()
 
         # debug(SCORE_COUNTER)
 
         # changeSpeed()
         # changeWave()
-        if keepGenerating == TRUE:
-            generateWave()
-        else:
+        if keepGenerating == FALSE:
             gameOver()
-
-        color = WAVE_COLOUR
-        # so i want to reach a fixed point in the screen that will be the collision area.
+        else:
+            generateWave()
 
         for Y_CORD in range(len(POINTS_LIST)):
 
@@ -269,7 +196,7 @@ def run_game():
                     print("Next Song")
                     songNum += 1
 
-        moveCircle()
+        BALL_CORD_X = moveCircle()
         drawCircle(GAME_DISPLAY, BALL_CORD_X, BALL_CORD_Y, BALL_RADIUS, WHITE)
 
         pygame.display.flip()
