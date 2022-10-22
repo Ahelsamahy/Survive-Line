0c8be8c8ade4abe80cdaa4a8503261524420f760
AhmedMahfouz <ahmelsamahy@gmail.com>
2022-10-22 15:39:34 +0200

    feat<>: create func to add points

--- waveFunc.py
+++ waveFunc.py
@@ -7,38 +7,6 @@ from defs import *
 POINTS_I = 0
 
 
-def generateWave():
-    global WAVE_CORD_X, SCORE_COUNTER, POINTS_I
-
-
-
-    if POINTS_I % 800 == 0:
-        POINTS_I = 0
-
-    WAVE_CORD_X = int((DISPLAY_H/2) + WAVE_AMPLITUDE*math.sin(WAVE_FREQUENCY *
-                      ((float(0)/-DISPLAY_W)*(2*math.pi) + (WAVE_SPEED*time.time()))))
-
-    # make the dot only to the right half of screen, elif for the left part
-    if WAVE_CORD_X-WAVE_AMPLITUDE-WAVE_GAP > DISPLAY_W:
-        WAVE_CORD_X = DISPLAY_W+50+WAVE_GAP
-    elif WAVE_CORD_X-350-WAVE_GAP > DISPLAY_W:
-        WAVE_CORD_X = DISPLAY_W+350+WAVE_GAP
-
-    # not to generate dot that will overlap the left line
-    if (WAVE_CORD_X < DISPLAY_W//2):
-        WAVE_CORD_X = DISPLAY_W//2
-
-    
-    if POINTS_LIST[-1] != 0:
-        POINTS_LIST.pop(0)
-        POINTS_LIST.append(WAVE_CORD_X)
-    else:
-        POINTS_LIST.pop(POINTS_I)
-        POINTS_LIST.insert(POINTS_I, WAVE_CORD_X)
-    # checkGap()
-    POINTS_I += 1
-
-
 def changeSpeed(Counter):
     global FPS, GAME_SPEED
     if(Counter % (100 * (GAME_SPEED//2)) == 0) and FPS < 100:
@@ -57,6 +25,38 @@ def changeWave(Counter, waveGap):
     return waveGap, WAVE_AMPLITUDE
 
 
+def generateWave():
+    global WAVE_CORD_X, SCORE_COUNTER, POINTS_I
+
+    if POINTS_I % 800 == 0:
+        POINTS_I = 0
+
+    WAVE_CORD_X = int((DISPLAY_H/2) + WAVE_AMPLITUDE*math.sin(WAVE_FREQUENCY *
+                      ((float(0)/-DISPLAY_W)*(2*math.pi) + (WAVE_SPEED*time.time()))))
+
+    # make the dot only to the right half of screen, elif for the left part
+    if WAVE_CORD_X-WAVE_AMPLITUDE-WAVE_GAP > DISPLAY_W:
+        WAVE_CORD_X = DISPLAY_W+50+WAVE_GAP
+    elif WAVE_CORD_X-350-WAVE_GAP > DISPLAY_W:
+        WAVE_CORD_X = DISPLAY_W+350+WAVE_GAP
+
+    # not to generate dot that will overlap the left line
+    if (WAVE_CORD_X < DISPLAY_W//2):
+        WAVE_CORD_X = DISPLAY_W//2
+
+    addPoint(POINTS_I,WAVE_CORD_X)
+    
+    checkGap()
+    POINTS_I += 1
+
+def addPoint(index,point):
+    if POINTS_LIST[-1] != 0:
+        POINTS_LIST.pop(0)
+        POINTS_LIST.append(point)
+    else:
+        POINTS_LIST.pop(index)
+        POINTS_LIST.insert(index, point)
+
 def checkGap():
     # gapDirection = True  # to right is true, left is false
     if (POINTS_I not in [0, 1]) and (POINTS_LIST[POINTS_I-1]-POINTS_LIST[POINTS_I] > 1):
@@ -65,7 +65,7 @@ def checkGap():
     elif (POINTS_I not in [0, 1]) and (POINTS_LIST[POINTS_I] - POINTS_LIST[POINTS_I-1] > 1):
         gap = (POINTS_LIST[POINTS_I] - POINTS_LIST[POINTS_I-1]) - 1
         fillGap(gap, True)
-        
+
 
 def fillGap(gap, gapDirection):
     # gapDirection to right is true, left is false
@@ -75,24 +75,21 @@ def fillGap(gap, gapDirection):
     if gap == 0:
         gap = 1
     insideY = POINTS_I
-    pointsArr = np.array(POINTS_LIST)
     if(gapDirection):
         # to move the point according to gap
-        POINTS_LIST.insert(POINTS_I + (gap), POINTS_LIST[POINTS_I])
+        addPoint(POINTS_I + (gap),POINTS_LIST[POINTS_I])
         POINTS_LIST[POINTS_I] = 0
-        # print(POINTS_LIST[POINTS_I-1][0],
-        #   POINTS_LIST[POINTS_I+gap][0])
-        # to fill the gap
         for x in range(POINTS_LIST[POINTS_I-1], POINTS_LIST[POINTS_I+gap], (gap//gap)):
-            POINTS_LIST[insideY] = x+1
+            addPoint(insideY,x+1)
+            # POINTS_LIST[insideY] = x+1
             if insideY < 799:
                 insideY += 1
     else:
-        POINTS_LIST.insert(POINTS_I + (gap), POINTS_LIST[POINTS_I])
+        addPoint(POINTS_I + (gap),POINTS_LIST[POINTS_I])
         POINTS_LIST[POINTS_I] = 0
-        # print(POINTS_LIST[POINTS_I-1][0],
-        #       POINTS_LIST[POINTS_I+gap][0])
         for x in range(POINTS_LIST[POINTS_I-1], POINTS_LIST[POINTS_I+gap], -(gap//gap)):
-            POINTS_LIST[insideY] = x
+            addPoint(insideY,x)
+            # POINTS_LIST[insideY] = x
             if insideY < 799:
                 insideY += 1
+    POINTS_I = insideY-1
