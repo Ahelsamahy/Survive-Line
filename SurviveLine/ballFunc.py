d78f8abd0b532d1737a8c64427cdd13e734c85b0
AhmedMahfouz <ahmelsamahy@gmail.com>
2022-11-26 22:11:20 +0100

    feat<>: defs for ball are now in the main class
    still will remove the reset all function from it and the collision

--- SurviveLine/ballFunc.py
+++ SurviveLine/ballFunc.py
@@ -4,29 +4,43 @@ from defs import *
 import pygame
 
 
-
-PARTICLES = []
-BALL_CORD_Y = 550
-BALL_CORD_X = DISPLAY_W//2
-BALL_MOVE_SPEED = 5
-BALL_RADIUS = 12
-
 class Ball(object):
-    def __init__(self, waveGap,loc, vel, timer,gameDisplay,X_POS):
+    PARTICLES = []
+    BALL_MOVE_SPEED = 5
+    BALL_RADIUS = 12
+
+    def __init__(self, waveGap, loc, vel, timer, gameDisplay, X_POS):
+        self.ballCordY = 550  # fixed as the ball doesn't go up or down
+        self.ballCordX = X_POS  # changes as the ball moves on this axes
+        self.moveSpeed = 5
+        self.radius = 12
         self.WaveGap = waveGap
         self.Loc = loc
         self.Vel = vel
         self.Timer = timer
         self.GameDisplay = gameDisplay
-        self.rect = pygame.Rect(self.X_POS, BALL_CORD_Y, BALL_RADIUS, BALL_RADIUS)
+        self.rect = pygame.Rect(
+            self.X_POS, self.ballCordY, self.radius*2, self.radius*2)
 
-    def drawCircle(self,SCREEN):
-        pygame.gfxdraw.aacircle(SCREEN, self.rect.x, BALL_CORD_Y, BALL_RADIUS, WHITE)
-        pygame.gfxdraw.filled_circle(SCREEN, self.rect.x, BALL_CORD_Y, BALL_RADIUS, WHITE)
+    def drawBall(self, SCREEN):
+        pygame.gfxdraw.aacircle(SCREEN, self.rect.x,BALL_CORD_Y, self.radius*2, WHITE)
+        pygame.gfxdraw.filled_circle(SCREEN, self.rect.x, BALL_CORD_Y, self.radius*2, WHITE)
 
-    def collision(self,ball):
+    def moveBall(self, right=True):
+        # if moveBall= true then move to right
+        if right == True:
+            self.ballCordX += self.moveSpeed
+        else:
+            self.ballCordX -= self.moveSpeed
+
+    def reset(self):
+        self.ballCordY = 550
+        self.ballCordX = DISPLAY_W//2
+
+    def collision(self, ball):
         global keepGenerating
-        ball = pygame.Rect(BALL_CORD_X, BALL_CORD_Y, BALL_RADIUS, BALL_RADIUS)
+        ball = pygame.Rect(self.ballCordX, self.ballCordY,
+                           self.radius*2, self.radius*2)
         for x in range(245, 255):
             if (POINTS_LIST[x] != 0) and (ball.right >= POINTS_LIST[x]-55-self.WaveGap):
                 print("hit from " + str(x) + " right")
@@ -35,20 +49,6 @@ class Ball(object):
             if (POINTS_LIST[x] != 0) and ball.left <= POINTS_LIST[x]-338+self.WaveGap:
                 print("hit from " + str(x) + " left")
                 return keepGenerating == False
-
-    def drawCircle(self,SCREEN, x, y, radius, color):
-        pygame.gfxdraw.aacircle(SCREEN, x, y, radius, color)
-        pygame.gfxdraw.filled_circle(SCREEN, x, y, radius, color)
-
-    def moveCircle(self,moveBall):
-        # if moveBall= true then move to right
-        global BALL_CORD_X
-
-        if moveBall==True and BALL_CORD_X + (BALL_RADIUS) < DISPLAY_W-4:
-            BALL_CORD_X += BALL_MOVE_SPEED
-        if (moveBall==False) and BALL_CORD_X > 0 + BALL_RADIUS+4:
-            BALL_CORD_X -= BALL_MOVE_SPEED
-        return BALL_CORD_X
 
     def generateParticles(self):
         PARTICLES.append([self.Loc, self.Vel, self.Timer])
@@ -71,6 +71,6 @@ def reset():
     POINTS_I = 0
     keepGenerating = True
     PARTICLES.clear()
-    SCORE_COUNTER == 0
-    WAVE_GAP == 0
-    return WAVE_GAP, SCORE_COUNTER, POINTS_LIST,POINTS_I
+    SCORE_COUNTER = 0
+    WAVE_GAP = 0
+    return WAVE_GAP, SCORE_COUNTER, POINTS_LIST, POINTS_I
