d5438f42efe4422910a908b4a6d690fb7809bd72
AhmedMahfouz <ahmelsamahy@gmail.com>
2022-11-29 21:52:18 +0100

    refactor<def.py>: move each component of game to its own class
    wave definitions are moved to the Wave class in waveFunc

--- SurviveLine/defs.py
+++ SurviveLine/defs.py
@@ -5,21 +5,17 @@ FPS = 60
 
 DATA_FONT_SIZE = 35
 DATA_FONT_COLOR = (255, 255, 255)
-background_color = (0, 0, 0)
+BACKGROUND_COLOUR = (0, 0, 0)
 
 
 WHITE = (255, 255, 255)
 WAVE_COLOUR = (178, 190, 181)
-WAVE_GAP = 0                        #Gap between two waves
 SCORE_COUNTER = 0
 GAME_TIME = 0
-GAME_COUNTER = 0
 GAME_SPEED = 2
 
-WAVE_AMPLITUDE = 50
-WAVE_FREQUENCY = 1
-WAVE_SPEED = 1
+WAVE_GAP = 0                        #Gap between two waves
+
 WAVE_CORD_X = 0
-POINTS_LIST = [0]*800
 POINTS_I = 0                        #index inside the list to iterate through
 keepGenerating = True
