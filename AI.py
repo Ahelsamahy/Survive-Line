b438777e71b4f59b9022419362ff82ef4662ee34
AhmedMahfouz <ahmelsamahy@gmail.com>
2022-12-01 01:14:30 +0100

    feat<AI.py>: add train_ai

--- AI.py
+++ AI.py
@@ -17,7 +17,7 @@ class SurviveLineGame:
         run = True
         clock = pygame.time.Clock()
         while run:
-            clock.tick(Game.)
+            clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
@@ -45,3 +45,44 @@ class SurviveLineGame:
             pygame.display.update()
 
         pygame.quit()
+
+    def train_ai(self, genome1, genome2, config):
+        net1 = neat.nn.FeedForwardNetwork.create(genome1, config)
+        net2 = neat.nn.FeedForwardNetwork.create(genome2, config)
+
+        run = True
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit()
+
+            output1 = net1.activate(
+                (self.ball.ballCordX, self.Wave.pointsList[245:255]))
+            decision1 = output1.index(max(output1))
+
+            if decision1 == 0:
+                pass
+            elif decision1 == 1:
+                self.game.moveBall(right=False)
+            else:
+                self.game.moveBall(right=False)
+
+            output2 = net2.activate(
+                (self.right_paddle.y, self.ball.y, abs(self.right_paddle.x - self.ball.x)))
+            decision2 = output2.index(max(output2))
+
+            if decision2 == 0:
+                pass
+            elif decision2 == 1:
+                self.game.move_paddle(left=False, up=True)
+            else:
+                self.game.move_paddle(left=False, up=False)
+
+            game_info = Game.loop()
+
+            Game.draw(draw_score=False, draw_hits=True)
+            pygame.display.update()
+
+            if game_info.left_score >= 1 or game_info.right_score >= 1 or game_info.left_hits > 50:
+                self.calculate_fitness(genome1, genome2, game_info)
+                break
