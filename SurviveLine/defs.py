61173692be3ef84ec0b5f91614701a958d57e2d9
AhmedMahfouz <ahmelsamahy@gmail.com>
2022-12-02 19:56:24 +0100

    feat<defs>: remove old defs
    will remove the whole file later

--- SurviveLine/defs.py
+++ SurviveLine/defs.py
@@ -1,20 +1,15 @@
 
 DISPLAY_W = 400
 DISPLAY_H = 800
-FPS = 60
 
 DATA_FONT_SIZE = 35
 DATA_FONT_COLOR = (255, 255, 255)
 BACKGROUND_COLOUR = (0, 0, 0)
 
-
 WHITE = (255, 255, 255)
 
 SCORE_COUNTER = 0
 GAME_SPEED = 2
-
 WAVE_GAP = 0                        #Gap between two waves
-
 WAVE_CORD_X = 0
 POINTS_I = 0                        #index inside the list to iterate through
-keepGenerating = True
